d3a712c10fc8c423c2678f800b45c783810514fd
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-02-23 05:32:25 +0900

    Test

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,7 +10,7 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found!" />
-      <h1>404: Not Found!</h1>
+      <h1>404: Not Foun!</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
   )
