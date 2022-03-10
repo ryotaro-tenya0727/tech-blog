37dc08104449369fa6392928bdc0d67f905355c4
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-03-11 03:45:28 +0900

    Fix: aboutページにgraphqlタグ追加

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { graphql } from "gatsby"
 import Layout from "./../components/layout"
 const About = ({ location }) => {
   return (
@@ -10,32 +9,3 @@ const About = ({ location }) => {
 }
 
 export default About
-
-export const pageQuery = graphql`
-  query BlogPostBySlug($id: String!) {
-    site {
-      siteMetadata {
-        title
-        siteUrl
-      }
-    }
-    mdx(id: { eq: $id }) {
-      id
-      body
-      slug
-      frontmatter {
-        title
-        date(formatString: "YYYY年 MM月 DD日")
-        description
-        image_url
-        tags
-      }
-      tableOfContents
-    }
-    tags: allMdx {
-      group(field: frontmatter___tags) {
-        fieldValue
-      }
-    }
-  }
-`
