61cf210c074feebaa154ee8378ee2052d82081ab
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-26 06:39:12 +0900

    ADD: sidekiqの使い方に関する記事追加

--- src/components/organisms/Toc.jsx
+++ src/components/organisms/Toc.jsx
@@ -24,7 +24,7 @@ const Items = ({ tableOfContents, depth }) => {
           }}
         >
           <Link to={item.url} className={toc_link}>
-            {`${item.title}`}
+            {`・${item.title}`}
           </Link>
           {item.items && (
             <Items tableOfContents={item.items} depth={depth + 1} />
